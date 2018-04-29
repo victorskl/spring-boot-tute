44e62c15b4290301129d5b97a75a107cf455676d
Victor San Kho Lin <victorlin21@gmail.com>
2018-04-29 13:03:45 +1000

    Added bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
 # spring-boot-tute
 
 Spring Boot Exercises
+
+### bootstrap 
+
+- To bootstrap Spring Boot Maven project skeleton structure
+
+```
+$ git clone https://github.com/victorskl/spring-boot-tute.git
+$ cd spring-boot-tute
+
+$ bash bootstrap.sh my-boot-app
+
+$ tree my-boot-app
+my-boot-app
+├── README.md
+├── pom.xml
+└── src
+    ├── main
+    │   ├── java
+    │   │   └── Application.java
+    │   └── resources
+    │       └── application.properties
+    └── test
+        ├── java
+        │   └── ApplicationTest.java
+        └── resources
+            └── application.properties
+
+7 directories, 6 files
+```
+
+- Alternatively, use the [https://start.spring.io](https://start.spring.io) or through [`spring` CLI](00-using-cli)
+
+```
+$ spring init -g com.sankholin my-boot-app
+
+$ tree my-boot-app
+my-boot-app
+├── mvnw
+├── mvnw.cmd
+├── pom.xml
+└── src
+    ├── main
+    │   ├── java
+    │   │   └── com
+    │   │       └── sankholin
+    │   │           └── mybootapp
+    │   │               └── DemoApplication.java
+    │   └── resources
+    │       └── application.properties
+    └── test
+        └── java
+            └── com
+                └── sankholin
+                    └── mybootapp
+                        └── DemoApplicationTests.java
+
+12 directories, 6 files
+```
---
+++ bootstrap.sh
@@ -0,0 +1,124 @@
+#!/usr/bin/env bash
+
+[ $# -eq 0 ] && { echo "Usage: $0 my-boot-app-name"; exit 1; }
+
+APP=$1
+
+mkdir -p ${APP}/src/main/java
+mkdir -p ${APP}/src/main/resources
+
+cat >${APP}/src/main/java/Application.java <<EOF
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+EOF
+
+cat >${APP}/src/main/resources/application.properties <<EOF
+info.app.encoding=@project.build.sourceEncoding@
+info.app.java.source=@java.version@
+info.app.java.target=@java.version@
+info.app.version=@project.version@
+info.app.name = @project.name@
+info.app.groupId = @project.groupId@
+info.app.artifactId = @project.artifactId@
+EOF
+
+mkdir -p ${APP}/src/test/java
+mkdir -p ${APP}/src/test/resources
+
+cat >${APP}/src/test/java/ApplicationTest.java <<EOF
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+	@Test
+	public void contextLoads() {
+	}
+}
+EOF
+
+cat >${APP}/src/test/resources/application.properties <<EOF
+# placeholder
+EOF
+
+cat >${APP}/README.md <<EOF
+# ${APP}
+My Spring Boot App
+EOF
+
+cat >${APP}/.gitignore <<EOF
+.DS_Store
+.idea
+*.iml
+target
+*.class
+*.jar
+*.war
+EOF
+
+cat >${APP}/pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+
+    <groupId>com.sankholin</groupId>
+    <artifactId>${APP}</artifactId>
+    <version>1.0-SNAPSHOT</version>
+    <name>My Spring Boot App</name>
+
+    <properties>
+		<project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
+		<project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
+		<java.version>1.8</java.version>
+    </properties>
+
+    <!-- https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#using-boot-maven -->
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>2.0.1.RELEASE</version>
+    </parent>
+
+    <dependencies>
+        <!-- spring boot starters -->
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-test</artifactId>
+            <scope>test</scope>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-actuator</artifactId>
+        </dependency>
+
+        <!-- others -->
+
+    </dependencies>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+
+</project>
+EOF
