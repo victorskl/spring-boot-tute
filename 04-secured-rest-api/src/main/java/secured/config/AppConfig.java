a831d74f95a610706933014763747c994f3c4292
Victor San Kho Lin <victorlin21@gmail.com>
2018-06-20 15:04:22 +1000

    Added Micrometer and Prometheus metrics monitoring

--- 04-secured-rest-api/src/main/java/secured/config/AppConfig.java
+++ 04-secured-rest-api/src/main/java/secured/config/AppConfig.java
@@ -3,11 +3,12 @@ package secured.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfig {
 
-  @Bean
+  @Bean @Primary
   public ObjectMapper mapper() {
     return new ObjectMapper();
   }
